33fb15620b0e44f16ccf5ac0e123781c8de0823d
dcrame2 <dcrame2@gmail.com>
2023-10-22 14:47:44 -0500

    initial upload

---
+++ src/Layout/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "./Navigation";
+import Footer from "./Footer/Index";
+
+function Layout() {
+  return (
+    <>
+      <Navigation />
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
---
+++ src/components/modules/MainHero/Index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Navigation from "@/Layout/Navigation/Index";
+
+function Layout() {
+  return <div>Layout</div>;
+}
+
+export default Layout;
