84759cf9e79b80d133787dd7c5bac30b36d582bd
dcrame2 <dcrame2@gmail.com>
2023-11-20 21:53:14 -0600

    faster

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -16,7 +16,7 @@ function Layout({ children }) {
             initial={{ opacity: 0, x: "-100%" }}
             exit={{ opacity: 0, x: "100%" }}
             animate={{ opacity: 1, x: "0%" }}
-            transition={{ duration: 0.3 }}
+            transition={{ duration: 0.5 }}
             key={router.asPath}
           >
             {children}
