81b0af017d09e1f69bbed7cd0c1c23637b65fe1b
dcrame2 <dcrame2@gmail.com>
2023-11-29 19:49:11 -0600

    update

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+
+      {/* <!-- Google tag (gtag.js) --> */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2T6VQ7F67C"
+      ></Script>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-2T6VQ7F67C');`,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
