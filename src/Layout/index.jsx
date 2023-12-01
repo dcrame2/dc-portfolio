72849d48e72bfb5f0fafc236341e841d382c7c60
dcrame2 <dcrame2@gmail.com>
2023-11-30 20:54:18 -0600

    handleexitcomplete for animate presence page trans

--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -1,30 +1,13 @@
 import Navigation from "./Navigation/Index";
 import Footer from "./Footer/Index";
 import StyledComponentsRegistry from "@/lib/registry";
-import { motion, AnimatePresence } from "framer-motion";
-import { useRouter } from "next/router";
 import { navData, footerData } from "@/data/Index";
 
 function Layout({ children }) {
-  const router = useRouter();
   return (
     <>
       <Navigation data={navData} />
-      <StyledComponentsRegistry>
-        <AnimatePresence mode="wait" initial={false}>
-          <motion.div
-            initial={{ opacity: 0, y: "100%" }}
-            exit={{ opacity: 0, y: "100%" }}
-            animate={{ opacity: 1, y: "0%" }}
-            transition={{ duration: 0.5 }}
-            key={router.asPath}
-          >
-            <AnimatePresence mode="wait" initial={true}>
-              {children}
-            </AnimatePresence>
-          </motion.div>
-        </AnimatePresence>
-      </StyledComponentsRegistry>
+      <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       <Footer data={footerData} />
     </>
   );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,48 @@
 import "@/styles/globals.css";
 // import Layout from '@/layout/Index';
 import Layout from "@/Layout";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  // The handler to smoothly scroll the element into view
+  const handExitComplete = () => {
+    if (typeof window !== "undefined") {
+      // setTimeout duration to match the transition of the motion div creating the animation effect
+      setTimeout(() => {
+        // hash from the url aka section
+        const hashId = window.location.hash;
+        if (hashId) {
+          // Use the hash to find the first element with that id
+          const element = document.querySelector(hashId);
+          if (element) {
+            // Smooth scroll to that element
+            element.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+              inline: "nearest",
+            });
+          }
+        }
+      }, 500);
+    }
+  };
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence mode="wait" onExitComplete={handExitComplete}>
+        <motion.div
+          key={router.route}
+          initial={{ opacity: 0, y: "100%" }}
+          exit={{ opacity: 0, y: "100%" }}
+          animate={{ opacity: 1, y: "0%" }}
+          transition={{ duration: 0.5 }}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
   );
 }
