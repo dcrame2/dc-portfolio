203e60463c39ea5ecca87d5e133cc66c550d026b
Dylan Cramer <dcramer@impactnetworking.com>
2023-12-06 11:46:54 -0600

    update initial load

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,28 +1,87 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// import Document, { Html, Head, Main, NextScript } from "next/document";
+// import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      {/* <!-- Google tag (gtag.js) --> */}
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-2T6VQ7F67C"
-      ></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', 'G-2T6VQ7F67C');`,
-        }}
-      />
-      <body>
-        <script>0</script>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head />
+//       {/* <!-- Google tag (gtag.js) --> */}
+//       <script
+//         async
+//         src="https://www.googletagmanager.com/gtag/js?id=G-2T6VQ7F67C"
+//       ></script>
+//       <script
+//         dangerouslySetInnerHTML={{
+//           __html: `window.dataLayer = window.dataLayer || [];
+//           function gtag(){dataLayer.push(arguments);}
+//           gtag('js', new Date());
+
+//           gtag('config', 'G-2T6VQ7F67C');`,
+//         }}
+//       />
+//       <body>
+//         <script>0</script>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import Script from "next/script";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2T6VQ7F67C"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+
+           gtag('config', 'G-2T6VQ7F67C');`,
+          }}
+        />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
